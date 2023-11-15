ef9493196a92fe63403b73bee67d07caad864c61
Dmitry Martynov <mdymailru@gmail.com>
2023-11-15 16:32:22 +0300

    Initial Commit

---
+++ DeNet/DeNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeNetApp.swift
+//  DeNet
+//
+//  Created by Dmitry Martynov on 15.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct DeNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
